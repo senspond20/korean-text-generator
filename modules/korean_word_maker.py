93b37b49058074af581acd9090bf07b02c2e757b
senspond20 <senspond@gmail.com>
2024-02-09 15:48:58 +0900

    requirments

--- modules/korean_word_maker.py
+++ modules/korean_word_maker.py
@@ -21,16 +21,17 @@ class KoreanWordMaker():
         with torch.no_grad():
             generated_ids = self.model.generate(
                 input_ids,
-                do_sample=True,          # False 이면 컨텍스트가 동일하면 결과값이 항상 같으
+                do_sample=True,          # False이면 컨텍스트가 동일하면 결과값이 항상 같으
                 min_length=50,
                 max_length=max_length,
                 no_repeat_ngram_size=3,  # 토큰이 3개 이상 반복될 경우 3번째 토큰 확률을 0으로
-                repetition_penalty=1.5,
+                repetition_penalty=1.5,  # 반복 패널티
                 pad_token_id=self.tokenizer.pad_token_id,
                 eos_token_id=self.tokenizer.eos_token_id,
                 bos_token_id=self.tokenizer.bos_token_id,
                 top_k=50,
-                temperature=1
+                temperature=1           # 창의성y
+
             )
 
         """토큰 익덱스를 가지고 문장생성"""
