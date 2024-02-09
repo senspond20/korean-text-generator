6f58c9a6d5bf38b0f633319fd7b1949fd9f5230d
senspond20 <senspond@gmail.com>
2024-02-09 16:57:59 +0900

    docker settins, readme

--- Home.py
+++ Home.py
@@ -1,21 +1,23 @@
 import streamlit as st
 from modules.korean_word_maker import KoreanWordMaker
 
-# 머신러닝 모델 캐싱
+# 캐싱
 @st.cache_data
-def loader():
+def model_loader():
     return KoreanWordMaker()
 
-def generate(kw, max_length, input_text):
-    return kw.make_text(max_length, input_text);
+
+def generate(model, max_length, input_text):
+    return model.make_text(max_length, input_text);
+
 
 def main():
+    model = model_loader()
     st.title("아무말 생성기")
-    kw = loader()
 
     input_text = st.text_input(label="한국어로 입력해주세요(외국어 모름)", value="")
     if st.button("아무말이나 만들어줘", type="primary"):
-        st.write(generate(kw, 200, input_text))
+        st.write(generate(model, 200, input_text))
         # st.write(input_text)
 
 if __name__ == "__main__":
--- modules/korean_word_maker.py
+++ modules/korean_word_maker.py
@@ -1,23 +1,39 @@
 from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast
 import torch
 
+
 class KoreanWordMaker():
+    _instance = None
+    model = None
+    tokenizer = None
 
     def __init__(self):
-        model = GPT2LMHeadModel.from_pretrained(
-            "skt/kogpt2-base-v2"
-        )
-        model.eval()
-        tokenizer = PreTrainedTokenizerFast.from_pretrained(
-            "skt/kogpt2-base-v2",
-            eos_token="</s>"
-        )
-        self.model = model
-        self.tokenizer = tokenizer
+        """ 생성자 (lazy 싱글톤 패턴) """
+        if not KoreanWordMaker._instance:
+            model = GPT2LMHeadModel.from_pretrained(
+                "skt/kogpt2-base-v2"
+            )
+            model.eval()
+            tokenizer = PreTrainedTokenizerFast.from_pretrained(
+                "skt/kogpt2-base-v2",
+                eos_token="</s>"
+            )
+            self.model = model
+            self.tokenizer = tokenizer
+        else:
+            self.getInstance()
+
+    @classmethod
+    def getInstance(cls):
+        if not cls._instance:
+            cls._instance = KoreanWordMaker()
+        return cls._instance
+
 
     def make_text(self, max_length, input_text):
+        """ 한국어 문장 생성 """
         input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
-        """그리디 서치"""
+        # 그리디 서치
         with torch.no_grad():
             generated_ids = self.model.generate(
                 input_ids,
@@ -26,13 +42,13 @@ class KoreanWordMaker():
                 max_length=max_length,
                 no_repeat_ngram_size=3,  # 토큰이 3개 이상 반복될 경우 3번째 토큰 확률을 0으로
                 repetition_penalty=1.5,  # 반복 패널티
-                pad_token_id=self.tokenizer.pad_token_id,
+                pad_token_id=self.tokenizer.eos_token_id,
                 eos_token_id=self.tokenizer.eos_token_id,
                 bos_token_id=self.tokenizer.bos_token_id,
                 top_k=50,
-                temperature=1           # 창의성y
+                temperature=1           # 창의성
 
             )
 
-        """토큰 익덱스를 가지고 문장생성"""
+        # 토크나이저로 디코딩 문장생성
         return self.tokenizer.decode([el.item() for el in generated_ids[0]])
